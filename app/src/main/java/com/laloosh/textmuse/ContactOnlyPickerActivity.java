eb3dcd5e0df99e063f8e539078554c5f84141a8c
William Cheng <will.cheng@gmail.com>
2015-04-28 17:18:55 -0700

    Searching for contacts on contacts only picker screen

--- app/src/main/java/com/laloosh/textmuse/ContactOnlyPickerActivity.java
+++ app/src/main/java/com/laloosh/textmuse/ContactOnlyPickerActivity.java
@@ -1,18 +1,23 @@
 package com.laloosh.textmuse;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.support.v7.widget.SearchView;
 
 import com.laloosh.textmuse.datamodel.TextMuseContact;
 import com.laloosh.textmuse.dialogs.PhoneNumberRemovedDialogFragment;
@@ -28,6 +33,7 @@ public class ContactOnlyPickerActivity extends ActionBarActivity implements Load
 
     private ContactsAdapter mAdapter;
     private ContactPickerState mState;
+    private String mSearchTerm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +65,93 @@ public class ContactOnlyPickerActivity extends ActionBarActivity implements Load
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_contact_only_picker, menu);
+
+        // Locate the search item
+        MenuItem searchItem = menu.findItem(R.id.menu_search);
+
+        // Retrieves the system search manager service
+        final SearchManager searchManager =
+                (SearchManager) this.getSystemService(Context.SEARCH_SERVICE);
+
+        // Retrieves the SearchView from the search menu item
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // Assign searchable info to SearchView.
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+
+//        final int textViewID = searchView.getContext().getResources().getIdentifier("android:id/search_src_text",null, null);
+//        final AutoCompleteTextView searchTextView = (AutoCompleteTextView) searchView.findViewById(textViewID);
+//        try {
+//            Field mCursorDrawableRes = TextView.class.getDeclaredField("mCursorDrawableRes");
+//            mCursorDrawableRes.setAccessible(true);
+//            mCursorDrawableRes.set(searchTextView, 0); //This sets the cursor resource ID to 0 or @null which will make it visible on white background
+//        } catch (Exception e) {}
+
+
+
+        // Set listeners for SearchView
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String queryText) {
+                // Nothing needs to happen when the user submits the search string
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                String newFilter = !TextUtils.isEmpty(newText) ? newText : null;
+                if (mSearchTerm == null && newFilter == null) {
+                    return true;
+                }
+
+                if (mSearchTerm != null && mSearchTerm.equals(newFilter)) {
+                    return true;
+                }
+
+                mSearchTerm = newFilter;
+
+                // Restarts the loader. This triggers onCreateLoader(), which builds the
+                // necessary content Uri from mSearchTerm.
+                getSupportLoaderManager().restartLoader(Queries.ContactsQuery.QUERY_ID, null, ContactOnlyPickerActivity.this);
+                return true;
+            }
+        });
+
+        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+
+                mSearchTerm = null;
+                getSupportLoaderManager().restartLoader(Queries.ContactsQuery.QUERY_ID, null, ContactOnlyPickerActivity.this);
+                return true;
+
+            }
+        });
+
+        if (mSearchTerm != null) {
+            // If search term is already set here then this fragment is
+            // being restored from a saved state and the search menu item
+            // needs to be expanded and populated again.
+
+            // Stores the search term (as it will be wiped out by
+            // onQueryTextChange() when the menu item is expanded).
+            final String savedSearchTerm = mSearchTerm;
+
+            // Expands the search menu item
+            MenuItemCompat.expandActionView(searchItem);
+
+            // Sets the SearchView to the previous search string
+            searchView.setQuery(savedSearchTerm, false);
+        }
+
         return true;
     }
 
@@ -101,7 +194,12 @@ public class ContactOnlyPickerActivity extends ActionBarActivity implements Load
         }
 
         Uri contentUri;
-        contentUri = Queries.ContactsQuery.CONTENT_URI;
+
+        if (mSearchTerm == null) {
+            contentUri = Queries.ContactsQuery.CONTENT_URI;
+        } else {
+            contentUri = Uri.withAppendedPath(Queries.ContactsQuery.FILTER_URI, Uri.encode(mSearchTerm));
+        }
 
         return new CursorLoader(this,
                 contentUri,
