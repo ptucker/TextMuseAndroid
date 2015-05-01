18b5551defe74f0ca89a0642a20ac8bae8e7f952
William Cheng <will.cheng@gmail.com>
2015-05-01 02:20:23 -0700

    Adding blue color instead of 2 greens

--- app/src/main/java/com/laloosh/textmuse/Constants.java
+++ app/src/main/java/com/laloosh/textmuse/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
     public static final String CONTACTS_FILE = "Contacts.Cache";
 
-    static final int[] COLOR_LIST = {0xffe76636, 0xff19ab67, 0xff00ac65};
+    static final int[] COLOR_LIST = {0xffe76636, 0xff19ab67, 0xff006dac};
 
     public static final int DEFAULT_NUMBER_RECENT_CONTACTS = 5;
 
